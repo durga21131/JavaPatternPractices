fb299766c182cf8c2adf71fda078499c61faf85c
durga21131 <durgaprasad21131@gmail.com>
2024-12-25 15:38:50 +0530

    new

--- halfPyramidLeft.java
+++ halfPyramidLeft.java
@@ -1,16 +1,24 @@
-public class reversehalfPyramidLeft {
-    public static void main(String[] args) {
+import java.util.Scanner;
+public class halfPyramidLeft {
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter no of rows or size of  Reverse Half Pyramid Left :");
-		int row=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no of rows or size of Half Left Pyramid(for better experiance you can enter 3 to 10) :");
+		int row = sc.nextInt();
 		// i for rows
 		// j for columns
-		for(int i=0;i<=row;i++) {
-			for(int j=1;j<=row-i;j++) {
-				System.out.print("*");
-				}
-	        System.out.println();
+		for (int i = 1; i <= row; i++) {
+			// for spaces
+			for (int j = 1; j <= row - i; j++) {
+				System.out.print(" ");
+			}
+			//for stars
+			for(int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+			System.out.println();
 		}
 	}
 }
+	
