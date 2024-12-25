eabac0d7174b7a45a1d517a508d11048a9db194b
durga21131 <durgaprasad21131@gmail.com>
2024-12-25 15:34:44 +0530

    new

---
+++ reverseHalfPyramidRight.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class reverseHalfPyramidRight {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no of rows or size of  Reverse Half Pyramid Right :");
+		int row = sc.nextInt();
+		// i for rows
+		// j for columns
+		for (int i = 1; i <= row; i++) {
+			// for spaces
+			for (int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			// for stars
+			for (int j = 0; j <= row - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
