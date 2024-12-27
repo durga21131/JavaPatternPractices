32a602fd87639e513a88b09d03c37851cb6f38a1
durga21131 <durgaprasad21131@gmail.com>
2024-12-27 21:14:29 +0530

    new

---
+++ Xsymbol.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Xsymbol {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input=sc.nextInt();
+		for(int i=1;i<=input;i++) {
+			for(int j=1; j<=input;j++) {
+				if(i==j || i+j==input+1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}System.out.println();
+		}
+	}
+
+}
