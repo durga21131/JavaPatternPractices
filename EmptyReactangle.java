db2d7b357e15dbb7d2eee8d1a44dfb5bd49a5f32
durga21131 <durgaprasad21131@gmail.com>
2024-12-27 20:59:00 +0530

    new

---
+++ EmptyReactangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EmptyRectangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		for (int i = 1; i <= input; i++) {
+			for (int j = 1; j <= input; j++) {
+				if (i == 1 || j == 1 || i == input || j == input) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ Triangel.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Triangel {
+
+	public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	 int input=sc.nextInt();
+	 for(int i=1;i<=input;i++) {
+		 for(int j=1;j<=(2*input)-1;j++) {
+			 if(i==input || i+j==input+1 || j-i==input-1 ) {
+				 System.out.print("*");
+			 }
+			 else {
+				 System.out.print(" ");
+			 }
+		 }System.out.println();
+	 }
+	}
+
+}
