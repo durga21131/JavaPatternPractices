98f9d38e1a35932421050d0d6fd87cbe68466a5f
durga21131 <durgaprasad21131@gmail.com>
2024-12-28 19:19:05 +0530

    new

--- RightPyramid.java
+++ RightPyramid.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 public class RightPyramid {
 	public static void main(String[] args) {
-		int input = 6;
+		Scanner sc=new Scanner(System.in);
+		int input = sc.nextInt();
 		for (int i = 1; i <= input; i++) {
 			for (int j = 1; j <= input - i; j++) {
 				System.out.print(" ");
