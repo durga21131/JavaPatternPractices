b754faf1406182a690862e3806e24fcacec9375e
durga21131 <durgaprasad21131@gmail.com>
2024-12-28 19:20:28 +0530

    new

--- ReversePyramid.java
+++ ReversePyramid.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 public class ReversePyramid {
 
 	public static void main(String[] args) {
-		int input=5;
+		Scanner sc=new Scanner(System.in);
+		int input=sc.nextInt();
 		for(int i=1;i<=input;i++) {
 			for(int j=1;j<i;j++) {
 				System.out.print(" ");
