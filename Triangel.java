f0e43013ae079d16fe388221a1d2b55b5cbed952
durga21131 <durgaprasad21131@gmail.com>
2024-12-28 19:21:56 +0530

    new

--- Triangel.java
+++ Triangel.java
@@ -7,10 +7,12 @@ public class Triangel {
 	 int input=sc.nextInt();
 	 for(int i=1;i<=input;i++) {
 		 for(int j=1;j<=(2*input)-1;j++) {
+			// for stars
 			 if(i==input || i+j==input+1 || j-i==input-1 ) {
 				 System.out.print("*");
 			 }
 			 else {
+				// for spaces
 				 System.out.print(" ");
 			 }
 		 }System.out.println();
