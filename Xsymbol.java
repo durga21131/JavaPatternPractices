40920b101ec4c8b81deb4393f5b02c0ca7ba2b60
durga21131 <durgaprasad21131@gmail.com>
2025-01-04 21:02:59 +0530

    new

--- Xsymbol.java
+++ Xsymbol.java
@@ -8,13 +8,11 @@ public class Xsymbol {
 		for(int i=1;i<=input;i++) {
 			for(int j=1; j<=input;j++) {
 				// For Stars
-				
 				if(i==j || i+j==input+1) {
 					System.out.print("*");
 				}else {
 					// For Spaces
-
-					System.out.print(" ");
+                   System.out.print(" ");
 				}
 			}System.out.println();
 		}
