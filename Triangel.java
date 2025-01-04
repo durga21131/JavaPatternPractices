6735d1ce316383df7bcb6e92968aec86578b9414
durga21131 <durgaprasad21131@gmail.com>
2025-01-04 21:00:31 +0530

    new

--- Triangel.java
+++ Triangel.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Triangel {
-
+     // Empty Triangle
 	public static void main(String[] args) {
 	 Scanner sc=new Scanner(System.in);
 	 int input=sc.nextInt();
