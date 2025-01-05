9cc9bbefa8a761598ce9e027c0d2906bdbfc4030
durga21131 <durgaprasad21131@gmail.com>
2025-01-05 20:47:55 +0530

    new

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -10,6 +10,7 @@ public class NumberPyramid {
 			for (int j = 1; j <= input - i; j++) {
 				System.out.print(" " + " ");
 			}
+		
 			// for decreasing numbers
 			for (int j = input; j >= input - i + 1; j--) {
 				System.out.print(" " + j);
@@ -18,7 +19,7 @@ public class NumberPyramid {
 			for (int j = input - i + 2; j <= input; j++) {
 				System.out.print(" " + j);
 			}
-
+             // for line break
 			System.out.println();
 		}
 	}
