e4187c9c3e3165ed22f5ec90073da48ef7462712
durga21131 <durgaprasad21131@gmail.com>
2025-02-02 21:49:19 +0530

    new

--- halfPyramidRight.java
+++ halfPyramidRight.java
@@ -6,7 +6,17 @@ public class halfPyramidRight {
 		Scanner sc = new Scanner(System.in);
 		int row = sc.nextInt();
 		// i for rows
-		// j for columns
+		// j for columnsfor (int i = 1; i <= row; i++) {
+			// for spaces
+			for (int j = 1; j <= row - i; j++) {
+				System.out.print(" ");
+			}
+			//for stars
+			for(int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+			System.out.println();
+		}
 		
 	}
 }
