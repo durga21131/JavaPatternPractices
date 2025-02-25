ce75b6bb97b4128456f6b43c24b09d70dfc1e461
durga21131 <durgaprasad21131@gmail.com>
2025-02-25 19:53:03 +0530

    new

--- halfPyramidLeft.java
+++ halfPyramidLeft.java
@@ -4,25 +4,7 @@ public class halfPyramidLeft {
 
 	public static void main(String[] args) {
 		halfPyramidLeft p=new halfPyramidLeft();
-		// p.m();
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter no of rows or size of Half Pyramid(for better experiance you can enter 3 to 10) :");
-        int row=sc.nextInt();
-        // i for rows
-        // j for columns
-        for (int i=1; i<=row;i++){
-        	for(int j=1;j<=i;j++) {
-        		System.out.print("*");
-        	}System.out.println();
-        }
-	}
-		public void  m() {
-        for(int i=1;i<=row;i++) {
-        	for(int j=1;j<=row;j++) {
-        		if(j<=i) {
-        			System.out.print("*");
-        		}
-        	}System.out.println();
+	
         }
 		}
 }
