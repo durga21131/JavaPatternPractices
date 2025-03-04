7650409ca8ab30907ce03f0b154374fb863edd36
durga21131 <durgaprasad21131@gmail.com>
2025-03-04 21:13:11 +0530

    new

--- NumberPyramid.java
+++ NumberPyramid.java
@@ -3,24 +3,7 @@ import java.util.Scanner;
 public class NumberPyramid {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
-		for (int i = 1; i <= input; i++) {
-			// For Spaces
-			for (int j = 1; j <= input - i; j++) {
-				System.out.print(" " + " ");
-			}
 		
-			// for decreasing numbers
-			for (int j = input; j >= input - i + 1; j--) {
-				System.out.print(" " + j);
-			}
-            // for increasing numbers
-			for (int j = input - i + 2; j <= input; j++) {
-				System.out.print(" " + j);
-			}
-             // for line break
-			System.out.println();
 		}
 	}
 }
